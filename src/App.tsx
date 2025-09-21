ca7d7272eb4fafbc96473b816ab527a994bdb6d1
louis <louis@lemoncloud.io>
2025-09-21 14:46:15 +0900

    feat(ui): add alert component and modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-500">
-          Hello, Tailwind CSS 4.0!
-        </h1>
+      <>
+        <div className="min-h-screen flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-blue-500">
+            Hello, Tailwind CSS 4.0!
+          </h1>
           <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-      </div>
-    </>
+            <Button onClick={() =>console.log('clicked!')}>Click me</Button>
+          </div>
+        </div>
+      </>
   )
 }
 
