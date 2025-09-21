aee5b0c7a12f9ec6a1c50fb73f6321fcf72bf9d0
louis <louis@lemoncloud.io>
2025-09-21 19:48:30 +0900

    feat(admin): add post management page with category filtering and toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from "@/shared/components/ui/sonner"
 import App from './App'
 import './index.css'
 
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
                 <App />
+                <Toaster />
                 <ReactQueryDevtools initialIsOpen={false} />
             </QueryClientProvider>
         </BrowserRouter>
